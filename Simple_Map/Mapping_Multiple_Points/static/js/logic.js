eaae11c3ad11fd759c6da166bc0ca456d24b80c0
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-27 18:26:15 -0700

    cityData used

--- Simple_Map/Mapping_Multiple_Points/static/js/logic.js
+++ Simple_Map/Mapping_Multiple_Points/static/js/logic.js
@@ -9,38 +9,8 @@
 // coordinate #2 = lng, third param = zoom level on a 0-18 scale
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// replaced simple one marker on LA to five most pop. cities array below
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
+// replaced simple one marker on LA to five most pop. from cities.js 
+let cityData = cities;
 
 // with cities array created; want to iterate through it
 // make one marker per city
