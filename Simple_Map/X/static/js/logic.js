4561ff63793aeab9e2c4a3a44c86661ff1992aee
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-27 17:19:12 -0700

    circle marker created

--- Simple_Map/X/static/js/logic.js
+++ Simple_Map/X/static/js/logic.js
@@ -9,16 +9,26 @@
 // coordinate #2 = lng, third param = zoom level on a 0-18 scale
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+
+// add a marker to the simple map
+// mapping a marker on LA with increased circle radius
+let marker = L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // create a tile layer for the map
 // used to load, display a tile layer on the map
 // leaflet documentation: https://leafletjs.com/index.html
 
 // assign tileLayer() method to var streets
 // reference the api given by leaflet
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // addTGo used to add the graymap obj tile layer to out let map above
 streets.addTo(map);
+
