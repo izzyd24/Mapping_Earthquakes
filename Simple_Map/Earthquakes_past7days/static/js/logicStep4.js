02b58079f6ad56d98e4904349137a87c0e43cc07
Izzy Diaz III <Izzy.DiazIII@lumentum.com>
2022-06-29 20:51:19 -0700

    final pre-challenge update

--- Simple_Map/Earthquakes_past7days/static/js/logicStep4.js
+++ Simple_Map/Earthquakes_past7days/static/js/logicStep4.js
@@ -1,3 +1,5 @@
+// Should be functioning up to the last section of 13.6/Pre-Challenge
+
 // create tile layer for background of map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
